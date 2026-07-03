a5d3413da9dd0774bfa510468b124cabe6d3ea57
TheLongNoodle <Oaleyraz@gmail.com>
2026-07-03 13:43:06 +0300

    Added gyro to schematic sheet, improved motor function

--- scripts/motor-example.py
+++ scripts/motor-example.py
@@ -16,46 +16,25 @@ lgpio.gpio_claim_output(h, EN)
 lgpio.gpio_write(h, EN, 0)
 
 # ---------------- CONFIG ----------------
-STEPS_PER_REV = 190
+STEPS_PER_REV = 1600
+DELAY = 0.002
 
-MAX_SPEED = 2000   # steps/sec
-MIN_SPEED = 200
-ACCEL = 5
-
-# ---------------- STEP FUNCTION ----------------
-def step_pulse(delay):
-    lgpio.gpio_write(h, STEP, 1)
-    time.sleep(delay)
-    lgpio.gpio_write(h, STEP, 0)
-    time.sleep(delay)
-
-# ---------------- MOVE FUNCTION ----------------
-def move(steps, direction=1):
-    lgpio.gpio_write(h, DIR, direction)
-
-    speed = MIN_SPEED
-
-    for _ in range(steps):
-        # accelerate smoothly
-        if speed < MAX_SPEED:
-            speed += ACCEL
-
-        delay = 1 / (2 * speed)  # half-period
-        step_pulse(delay)
-
-# ---------------- HIGH LEVEL ----------------
+# ---------------- ROTATE ----------------
 def rotate(revolutions, direction=1):
-    steps = int(revolutions * STEPS_PER_REV)
-    move(steps, direction)
+    lgpio.gpio_write(h, DIR, direction)
+    for _ in int(revolutions * STEPS_PER_REV):
+        lgpio.gpio_write(h, STEP, 1)
+        time.sleep(DELAY)
+        lgpio.gpio_write(h, STEP, 0)
+        time.sleep(DELAY)
 
 # ---------------- RUN TEST ----------------
 try:
     print("Running motor...")
-
-    rotate(50, 1)
-    time.sleep(1)
-    rotate(50, 0)
+    rotate(1, 1)
+    print("Done!")
 
 finally:
     lgpio.gpio_write(h, EN, 1)  # disable motor
     lgpio.gpiochip_close(h)
+    print("Task finished!")
